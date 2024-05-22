125cf110bd4f87bc80ccf827e39ad76ba4583fb6
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-05-22 15:23:50 -0500

    fix: txt records

--- src/gopappy/api.py
+++ src/gopappy/api.py
@@ -5,34 +5,34 @@ from gopappy.auth import main as auth
 
 API_KEY, API_SECRET = auth()
 
-headers = {
-    'Authorization': 'sso-key {}:{}'.format(API_KEY,
-                                            API_SECRET
-    )
-}
+default_headers = {"Authorization": "sso-key {}:{}".format(API_KEY, API_SECRET)}
+
 
 class API:
-    api_url = 'https://api.godaddy.com/v1'
+    api_url = "https://api.godaddy.com/v1"
     _shared = None
 
     def __init__(self, key, secret):
         self.key = key
         self.secret = secret
+        self.default_headers = {"Authorization": f"sso-key {self.key}:{self.secret}"}
 
     @classmethod
     def shared(cls):
         if not cls._shared:
-            cls._shared = API(None, None)
+            cls._shared = API(API_KEY, API_SECRET)
         return cls._shared
 
     def get(self, path, **params):
-        url = '{}/{}'.format(self.api_url, path)
-        return requests.get(url, headers=headers, params=params)
+        url = f"{self.api_url}/{path}"
+        return requests.get(url, headers=self.default_headers, params=params)
 
     def patch(self, path, **kwargs):
-        url = '{}/{}'.format(self.api_url, path)
+        url = f"{self.api_url}/{path}"
+        headers = kwargs.pop('headers', {})  # Remove headers from kwargs
+        headers.update(self.default_headers)  # Merge with default headers
         return requests.patch(url, headers=headers, **kwargs)
 
     def delete(self, path):
-        url = '{}/{}'.format(self.api_url, path)
-        return requests.delete(url, headers=headers)
+        url = f"{self.api_url}/{path}"
+        return requests.delete(url, headers=self.default_headers)
--- src/gopappy/cli.py
+++ src/gopappy/cli.py
@@ -13,120 +13,80 @@ app = typer.Typer()
 
 
 @app.command()
-def domain(
-    domain: str = typer.Argument(...,
-                                 help="Domain to be managed. e.g. mydomain.com"),
-    action: str = typer.Argument(...,
-                                 help="What to do with the domain"),
-    data: Optional[list[str]] = typer.Argument(None,
-                                               help="""add-record: [type, name, data]
-                                               delete-record: [type, name]"""),
-    only_type: Optional[str] = typer.Option(None,
-                                            "--type", "-t",
-                                            help="Filter by record type"),
+def records(
+    domain: str = typer.Argument(..., help="Domain to be managed. e.g. mydomain.com"),
+    only_type: Optional[str] = typer.Option(None, "--type", "-t", help="Filter by record type"),
 ):
     api = API.shared()
+    response = api.get(f"domains/{domain}/records")
+    records = response.json()
 
-    if action == "records":
-        response = api.get("domains/{}/records".format(domain))
-        data = response.json()
+    max_name_length = max(len(record["name"]) for record in records) if records else 0
+    name_width = max(max_name_length, len("Name"))
 
-        try:
-            for record in data:
-                if not only_type or record["type"].lower() == only_type.lower():
-                    print(f"{record['type']: <10}{record['name']: <20}{record['data']}")
-        except TypeError as e:
-            colorize("red", f"Domain:\t{domain}")
-            colorize("red", f"Error:\t{e}")
-
-    elif action == "add-record":
-        url = "domains/{}/records".format(domain)
-        params = [
-            {
-                "type": data[0].upper(),    # A / CNAME
-                "name": data[1],            # fulano., mangano.
-                "data": data[2],            # points to ip/domain
-            }
-        ]
-        response = api.patch(url, data=json.dumps(params))
-
-        if response.status_code != 200:
-            info = response.json()
-            print(info["code"], file=sys.stderr)
-            sys.exit(1)
-
-    elif action == "delete-record":
-        if data and len(data) >= 2:
-            record_type, record_name = data[:2]
-            confirm = typer.confirm("Are you sure you want to delete this record?")
-            if confirm:
-                url = f"domains/{domain}/records/{record_type}/{record_name}"
-                response = api.delete(url)
-                if response.status_code != 200:
-                    try:
-                        info = response.json()
-                        print(info["code"], file=sys.stderr)
-                    except ValueError:
-                        print(f"Failed to decode JSON response: {response.text}", file=sys.stderr)
-                else:
-                    print("Record deleted successfully.")
-        else:
-            print("Insufficient data provided for deleting the record. Please provide [type, name].", file=sys.stderr)
-
-    elif action == "suggest":
-        url = "domains/suggest"
-        domains = data[0].split(",")
-        response = api.get(url, tlds=domains)
-
-    elif action == "available" or action == "check":
-        response = api.get("domains/available", domain=domain)
-        data = response.json()
-        if data["available"]:
-            print("available")
-        else:
-            print("not available")
-
-    else:
-        print("Unsupported action")
+    print(f"Domain: {domain}")
+    try:
+        for record in records:
+            if not only_type or record["type"].lower() == only_type.lower():
+                print(f"{record['type']: <10}{record['name']: <{name_width}}{record['data']}")
+    except TypeError as e:
+        colorize("red", f"Domain:\t{domain}")
+        colorize("red", f"Error:\t{e}")
 
 
 @app.command()
-def domains(
-    status: str = typer.Option("active",
-                               help="Filter by domain status")
+def add_record(
+    domain: str = typer.Argument(..., help="Domain to be managed. e.g. mydomain.com"),
+    type_: str = typer.Option(..., "--type", "-t", help="Record type"),
+    name: str = typer.Option(..., "--name", "-n", help="Record name"),
+    data: str = typer.Option(..., "--data", "-d", help="Record data"),
 ):
     api = API.shared()
-    data = api.get('/domains')
-    status = status.upper() if status else ''
-    for domain in data.json():
-        if status != '':
-            if domain['status'] == status:
-                colorize("green", f"[DOMAIN] {domain['domain']}")
-                colorize("green", f"[STATUS] {domain['status']}")
-            else:
-                colorize("green", f"[DOMAIN] {domain['domain']}")
-                colorize("red", f"[STATUS] {domain['status']}")
-        else:
-            colorize("white", f"[DOMAIN] {domain['domain']}")
-            colorize("white", f"[STATUS] {domain['status']}")
+    url = f"domains/{domain}/records"
+    record_data = [{"type": type_.upper(), "name": name, "data": data}]
+    response = api.patch(url, json=record_data)
+
+    print(f"Domain: {domain}")
+    print(f"Type: {type_}")
+    print(f"Name: {name}")
+    print(f"Data: {data}")
+
+    if response.status_code != 200:
+        try:
+            info = response.json()
+            print(info["code"], file=sys.stderr)
+        except ValueError:
+            print(f"Failed to decode JSON response: {response.text}", file=sys.stderr)
+        sys.exit(1)
+    else:
+        print("Record added successfully.")
 
 
-
-def version_callback(value: bool):
-    if value:
-        print(__version__)
-        raise typer.Exit()
-
-
-@app.callback()
-def version(
-    version: bool = typer.Option(None,
-                                 "--version", "-v",
-                                 callback=version_callback,
-                                 is_eager=True,
-                                 help="Print the version and exit")
+@app.command()
+def delete_record(
+    domain: str = typer.Argument(..., help="Domain to be managed. e.g. mydomain.com"),
+    type_: str = typer.Option(..., "--type", "-t", help="Record type"),
+    name: str = typer.Option(..., "--name", "-n", help="Record name"),
 ):
-    pass
+    api = API.shared()
+    confirm = typer.confirm("Are you sure you want to delete this record?")
+    if confirm:
+        url = f"domains/{domain}/records/{type_}/{name}"
+        response = api.delete(url)
+
+        print(f"Domain: {domain}")
+        print(f"Type: {type_}")
+        print(f"Name: {name}")
+
+        if response.status_code != 200:
+            try:
+                info = response.json()
+                print(info["code"], file=sys.stderr)
+            except ValueError:
+                print(f"Failed to decode JSON response: {response.text}", file=sys.stderr)
+            sys.exit(1)
+        else:
+            print("Record deleted successfully.")
 
 
 if __name__ == "__main__":
